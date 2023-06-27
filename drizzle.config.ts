e85b6f92c925a22437ac1c0e2234fcf3a5de1a59
Giacomo Rebonato <grebonato@outlook.ie>
2023-06-27 16:26:50 +0100

    update migrations path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'drizzle-kit'
 
 export default {
-  out: './server/migrations',
+  out: './migrations',
   schema: './server/db/schema.ts',
   breakpoints: true,
 } satisfies Config
--- server/db/db.ts
+++ server/db/db.ts
@@ -14,7 +14,7 @@ export const db = drizzle(client)
 
 try {
   await migrate(db as any, {
-    migrationsFolder: Path.join(appRoot.path, 'server', 'migrations'),
+    migrationsFolder: Path.join(appRoot.path, 'migrations'),
   })
 } catch (error) {
   console.error('Migration failed')
