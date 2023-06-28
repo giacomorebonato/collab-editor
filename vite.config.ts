fa3b0e14501e527544741c5f17d07deb473ff457
Giacomo Rebonato <grebonato@outlook.ie>
2023-06-28 14:15:22 +0100

    cache everything

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      workbox: {
+        globPatterns: ['**/*'],
+      },
       includeAssets: ['**/*'],
       outDir: './dist/assets',
     }),
