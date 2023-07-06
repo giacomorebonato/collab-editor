c9d094c87d5c4378cc472ca5d19bc213c03c710c
Giacomo Rebonato <grebonato@outlook.ie>
2023-07-06 20:25:30 +0100

    add icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hiijeh4c1hg"
+    "revision": "0.q9d5g9mjf78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
