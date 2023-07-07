50efbeab2737a0e168a3c03368e965b31baa22b7
Giacomo Rebonato <grebonato@outlook.ie>
2023-07-07 08:44:56 +0100

    handling icons in root

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,21 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.*'],
       },
+      manifest: {
+        name: 'collab-editor',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }),
   ],
 })
